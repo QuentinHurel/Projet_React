4cbe1eeb51b1f50ffe117c09d3619853f8b2f269
Quentin Hurel <hurel.quentin@hotmail.com>
2018-10-04 20:32:26 +0200

    commit d'un peu de style parce que j'ai que ça à faire

--- Component/Accueil.js
+++ Component/Accueil.js
@@ -1,22 +1,47 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 class Accueil extends React.Component {
 
-    _gameScreen () {
+    _gameScreen() {
         this.props.navigation.navigate("Game")
     }
 
     render() {
 
         return (
-            <View>
-                <TouchableOpacity style={{width: 100, height:100}} onPress={() => this._gameScreen()}>
-                    <Text>Game</Text>
+            <View sytyle={styles.container}>
+                <TouchableOpacity style={styles.toucheable} onPress={() => this._gameScreen()}>
+                    <Text style={styles.text}>Jouer</Text>
                 </TouchableOpacity>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    toucheable: {
+        marginTop: 400,
+        borderColor: 'black',
+        width: 200,
+        marginLeft: 105,
+        backgroundColor: '#323840',
+        shadowOpacity: 1,
+        borderRadius: 10,
+        elevation: 10
+    },
+    text: {
+        fontStyle: ('normal', 'italic'),
+        textAlign: 'center',
+        color: '#EEEEEE',
+        padding: 25
+    }
+})
 
 export default Accueil
--- Component/Game.js
+++ Component/Game.js
@@ -1,34 +1,46 @@
 import React from 'react'
-import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native'
 
-class Game extends React.Component  {
+class Game extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <View>
-                    <TouchableOpacity style={[styles.touchable, { backgroundColor: 'red' }]}></TouchableOpacity>
-                    <TouchableOpacity style={[styles.touchable, { backgroundColor: 'green' }]}></TouchableOpacity>
+      
+                <View style={styles.cubebox}>
+                    <View>
+                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'red', marginTop: 50, marginRight: 25 }]} ></TouchableOpacity>
+                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'green', marginRight: 25 }]}></TouchableOpacity>
+                    </View>
+                    <View>
+                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'blue', marginTop: 50 }]}></TouchableOpacity>
+                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'yellow' }]}></TouchableOpacity>
+                    </View>
                 </View>
-                <View>
-                    <TouchableOpacity style={[styles.touchable, { backgroundColor: 'blue' }]}></TouchableOpacity>
-                    <TouchableOpacity style={[styles.touchable, { backgroundColor: 'yellow' }]}></TouchableOpacity>
-                </View>
-            </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    cubebox: {
         flex: 1,
         flexDirection: 'row',
+        alignItems: 'center',
+        height:10,
+        justifyContent: 'center',
         flexWrap: 'wrap',
         backgroundColor: '#fff'
     },
-    touchable:  {
+    touchable: {
         flex: 1,
-        width: 200,
-        height: 100
+        width: 150,
+        marginBottom: 40,
+        borderRadius: 10,
+        borderWidth: 10,
+        borderColor: 'black'
+    },
+    text: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
