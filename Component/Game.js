bac2adf1a5706735ec07b47b3daa2983d20d120e
qhurel <qhurel@umanis.com>
2018-11-15 16:02:36 +0100

    ajout random et values cases

--- Component/Game.js
+++ Component/Game.js
@@ -2,17 +2,43 @@ import React from 'react'
 import { View, TouchableOpacity, StyleSheet, Text } from 'react-native'
 
 class Game extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            red: 1,
+            blue: 2,
+            green: 3,
+            yellow: 4
+        }
+        numberStock = [];
+        iteration = 3;
+    }
+
+    generateRandomNumber = () =>{
+        for(let i = 0; i < iteration; i++){
+            let random = Math.floor(Math.random() * (5 - 1) + 1);
+            numberStock.add(random);
+        }
+        return numberStock;
+    }
+
+    getValue = (x) => {
+        console.log(this.state.x);
+    }
+
+    
     render() {
         return (
       
                 <View style={styles.cubebox}>
                     <View>
-                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'red', marginTop: 50, marginRight: 25 }]} ></TouchableOpacity>
-                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'green', marginRight: 25 }]}></TouchableOpacity>
+                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'red', marginTop: 50, marginRight: 25 }]}  onPress={(red) => this.getValue(red)}></TouchableOpacity>
+                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'green', marginRight: 25 }]} value={this.state.green} onPress={(green) => this.getValue(green)}></TouchableOpacity>
                     </View>
                     <View>
-                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'blue', marginTop: 50 }]}></TouchableOpacity>
-                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'yellow' }]}></TouchableOpacity>
+                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'blue', marginTop: 50 }]} value={this.state.blue} onPress={(blue) => this.getValue(blue)}></TouchableOpacity>
+                        <TouchableOpacity style={[styles.touchable, { backgroundColor: 'yellow' }]} value={this.state.yellow} onPress={(yellow) => this.getValue(yellow)}></TouchableOpacity>
                     </View>
                 </View>
         )
