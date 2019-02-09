548ad2ea5cc2c2da955b1d6ac438c8c8ef518374
Hurel <hurel.quentin@hotmail.com>
2019-02-09 14:47:01 +0100

    ajout composant résultat

---
+++ Component/Result.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class Result extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Page de résultat</Text>
+            </View>
+        )
+    }
+}
+
+export default Result
